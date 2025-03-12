d2199e688ab920271f3767b2311e5dcb72fd43d1
Alvaro Angel <alvaroangelm@gmail.com>
2025-03-11 20:29:51 -0500

    ...

--- 06-Estadística-Y-Probabilidad/Pensamiento-Aleatorio/06-Medidas-De-Posición/SemilleroUnico.R
+++ 06-Estadística-Y-Probabilidad/Pensamiento-Aleatorio/06-Medidas-De-Posición/SemilleroUnico.R
@@ -3,7 +3,7 @@ library(exams)
 
 # Definición del archivo de examen y configuración inicial
 archivo_examen <- "cuartil-estatura.Rmd"
-archivos <- 3
+archivos <- 400
 numpreg <- 1
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
