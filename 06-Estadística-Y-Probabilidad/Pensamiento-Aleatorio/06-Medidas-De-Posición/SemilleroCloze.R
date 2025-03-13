8749bd0f972b42675eb952f438ee9819e852907e
Alvaro Angel <alvaroangelm@gmail.com>
2025-03-12 21:37:53 -0500

    Escalando propuetas con 'cuartil-estatura-cloze.Rmd'

---
+++ 06-Estadística-Y-Probabilidad/Pensamiento-Aleatorio/06-Medidas-De-Posición/SemilleroCloze.R
@@ -0,0 +1,26 @@
+library(exams)
+
+# Definición del archivo de examen y configuración
+archivo_examen <- "cuartil-estatura-cloze.Rmd"  # Asegúrate de que el archivo tenga este nombre
+archivos <- 4
+semilla <- sample(100:1e8, 1)
+set.seed(semilla)
+dir_salida <- "salida"
+dir_ejercicios <- "ejercicios"
+
+# Nombre del archivo sin la extensión .Rmd
+nombre_sin_extension <- sub("\\.Rmd$", "", archivo_examen)
+nombre_arch <- paste0(nombre_sin_extension, "_")
+
+################################################################################
+# Generación para Moodle - configuración básica
+
+set.seed(semilla)
+exams2moodle(archivo_examen,
+             n = archivos,
+             svg = TRUE,
+             name = nombre_arch,
+             encoding = "UTF-8",
+             dir = "salida",
+             edir = "ejercicios")
+################################################################################
--- 06-Estadística-Y-Probabilidad/Pensamiento-Aleatorio/06-Medidas-De-Posición/SemilleroUnico2.R
+++ 06-Estadística-Y-Probabilidad/Pensamiento-Aleatorio/06-Medidas-De-Posición/SemilleroUnico2.R
@@ -2,7 +2,7 @@ library(exams)
 library(knitr)  # Explicitly load knitr
 
 # Definición del archivo de examen y configuración inicial
-archivo_examen <- "cuartil-estatura-02.Rmd"
+archivo_examen <- "cuartil-estatura-mchoice.Rmd"
 archivos <- 1
 numpreg <- 5
 semilla <- sample(100:1e8, 1)
