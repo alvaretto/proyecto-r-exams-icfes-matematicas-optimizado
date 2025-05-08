ca2eb10e03f4f14401ed0121928dace3532e3ebc
alvaretto <alvaroangelm@gmail.com>
2025-05-08 16:44:24 -0500

    Generando versiones para “01-S2-2025-SEDQ

--- Lab/36/SemilleroUnico.R
+++ Lab/36/SemilleroUnico.R
@@ -4,7 +4,7 @@ library(exams)
 # Definición del archivo de examen y configuración inicial
 archivo_examen <- "volumen_cilindro_hueco_py_v1.Rmd"
 copias <- 1
-numpreg <- 5
+numpreg <- 20
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
 dir_salida <- "salida"
@@ -90,18 +90,18 @@ exams2html(rep(archivo_examen, numpreg),
 # Generación para Moodle, solo configura manualmente 'copias'
 # no importa 'numpreg'
 
-set.seed(semilla)
-exams2moodle(archivo_examen,
-             n = copias,
-             svg = TRUE,
-             name = nombre_arch,
-             encoding = "UTF-8",
-             dir = "salida",
-             edir = "ejercicios",
-             mchoice = list(shuffle = TRUE,
-                            answernumbering = "ABCD",
-                            eval = list(partial = TRUE,
-                                        rule = "none")))
+# set.seed(semilla)
+# exams2moodle(archivo_examen,
+#              n = copias,
+#              svg = TRUE,
+#              name = nombre_arch,
+#              encoding = "UTF-8",
+#              dir = "salida",
+#              edir = "ejercicios",
+#              mchoice = list(shuffle = TRUE,
+#                             answernumbering = "ABCD",
+#                             eval = list(partial = TRUE,
+#                                         rule = "none")))
 
 ################################################################################
 # Generación para NOPS (exámenes escaneables)
