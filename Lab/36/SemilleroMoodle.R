a0f6ad0e769618180a93954c8f72dd42bd5f0e69
alvaretto <alvaroangelm@gmail.com>
2025-05-07 20:53:43 -0500

    Optimizando 'volumen_cilindro_hueco_py_v1.Rmd'

--- Lab/36/SemilleroMoodle.R
+++ Lab/36/SemilleroMoodle.R
@@ -3,7 +3,7 @@ library(exams)
 
 # Definición del archivo de examen y configuración inicial
 archivo_examen <- "volumen_cilindro_hueco_py_v1.Rmd"
-copias <- 5
+copias <- 250
 numpreg <- 1
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
