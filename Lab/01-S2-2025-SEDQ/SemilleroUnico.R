7ec6338dd260c34cd4c99482828c806b30e346b1
alvaretto <alvaroangelm@gmail.com>
2025-05-08 22:21:12 -0500

    Generando versiones para “01-S2-2025-SEDQ

--- Lab/01-S2-2025-SEDQ/SemilleroUnico.R
+++ Lab/01-S2-2025-SEDQ/SemilleroUnico.R
@@ -4,7 +4,7 @@ library(exams)
 # Definición del archivo de examen y configuración inicial
 archivo_examen <- "01-S2-2025-SEDQ-v1.Rmd"
 copias <- 1
-numpreg <- 3
+numpreg <- 5
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
 dir_salida <- "salida"
