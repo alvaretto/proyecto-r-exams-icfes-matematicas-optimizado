232e72898ba2600620d0c407da69ba4000b6585a
alvaretto <alvaroangelm@gmail.com>
2025-05-09 22:51:08 -0500

    Generando “01-S2-2025-SEDQ-v3.Rmd

--- Lab/01-S2-2025-SEDQ/SemilleroUnico.R
+++ Lab/01-S2-2025-SEDQ/SemilleroUnico.R
@@ -2,9 +2,9 @@
 library(exams)
 
 # Definición del archivo de examen y configuración inicial
-archivo_examen <- "grafico_circular_bienes_v2.Rmd"
+archivo_examen <- "grafico_circular_bienes_v3.Rmd"
 copias <- 1
-numpreg <- 5
+numpreg <- 3
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
 dir_salida <- "salida"
