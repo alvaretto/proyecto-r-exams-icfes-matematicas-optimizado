04ac38af94f7cce343660caaad8f50e8b5e37b6d
alvaretto <alvaroangelm@gmail.com>
2025-05-11 21:30:29 -0500

    Generando 02-S2-2025-SEDQ-grafico_circular_bienes_cloze_v1.Rmd

--- Lab/01-S2-2025-SEDQ/SemilleroUnico.R
+++ Lab/01-S2-2025-SEDQ/SemilleroUnico.R
@@ -4,7 +4,7 @@ library(exams)
 # Definición del archivo de examen y configuración inicial
 archivo_examen <- "grafico_circular_bienes_v0.Rmd"
 copias <- 1
-numpreg <- 5
+numpreg <- 10
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
 dir_salida <- "salida"
