ba66d978466276ed17fe62158888217f5e83a3df
alvaretto <alvaroangelm@gmail.com>
2025-05-12 10:18:22 -0500

    Corrigiendo 02-S2-2025-SEDQ-grafico_circular_bienes_cloze_v1.Rmd

--- Lab/01-S2-2025-SEDQ/SemilleroUnico.R
+++ Lab/01-S2-2025-SEDQ/SemilleroUnico.R
@@ -2,9 +2,9 @@
 library(exams)
 
 # Definición del archivo de examen y configuración inicial
-archivo_examen <- "grafico_circular_bienes_v0.Rmd"
+archivo_examen <- "02-S2-2025-SEDQ-grafico_circular_bienes_cloze_v1.Rmd"
 copias <- 1
-numpreg <- 10
+numpreg <- 3
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
 dir_salida <- "salida"
@@ -90,18 +90,18 @@ exams2html(rep(archivo_examen, numpreg),
 # Generación para Moodle, solo configura manualmente 'copias'
 # no importa 'numpreg'
 
-# set.seed(semilla)
-# exams2moodle(archivo_examen,
-#              n = copias,
-#              svg = TRUE,
-#              name = nombre_arch,
-#              encoding = "UTF-8",
-#              dir = "salida",
-#              edir = "ejercicios",
-#              mchoice = list(shuffle = TRUE,
-#                             answernumbering = "ABCD",
-#                             eval = list(partial = TRUE,
-#                                         rule = "none")))
+set.seed(semilla)
+exams2moodle(archivo_examen,
+             n = copias,
+             svg = TRUE,
+             name = nombre_arch,
+             encoding = "UTF-8",
+             dir = "salida",
+             edir = "ejercicios",
+             mchoice = list(shuffle = TRUE,
+                            answernumbering = "ABCD",
+                            eval = list(partial = TRUE,
+                                        rule = "none")))
 
 ################################################################################
 # Generación para NOPS (exámenes escaneables)
