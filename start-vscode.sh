19cd11a9427b5273e5b7943d339df34776211d06
Álvaro Ángel Molina <37968648+alvaretto@users.noreply.github.com>
2025-05-24 19:59:41 -0500

    Borrando
    Borrando

--- start-vscode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Limpiar el directorio de extensiones al inicio
-rm -rf /home/coder/.local/share/code-server/extensions/*
-
-# Iniciar code-server con los argumentos predeterminados
-exec /usr/bin/entrypoint.sh "$@"
