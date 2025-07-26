7e5b99668eeb4885f5946f187c4616b1a26af361
Álvaro Ángel Molina <37968648+alvaretto@users.noreply.github.com>
2025-07-25 21:22:04 -0500

    Optimizando I_1796473-Opc-C2_optimizado.Rmd

--- Auxiliares/Ejemplos-Funcionales-Rmd/Plantillas/tex/Graficos_Estadisticos_Adopcion_Mascotas/SemilleroUnico_Oficio_v1.R
+++ Auxiliares/Ejemplos-Funcionales-Rmd/Plantillas/tex/Graficos_Estadisticos_Adopcion_Mascotas/SemilleroUnico_Oficio_v1.R
@@ -5,9 +5,9 @@ library(exams)
 .exams_generation_mode <- TRUE
 
 # Definición del archivo de examen y configuración inicial
-archivo_examen <- "I_1796473-Opc-D2_optimizado.Rmd"
+archivo_examen <- "I_1796473-Opc-C2_optimizado.Rmd"
 copias <- 1
-numpreg <- 3
+numpreg <- 5
 semilla <- sample(100:1e8, 1)
 set.seed(semilla)
 dir_salida <- "salida"
