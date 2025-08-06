156a9b0ab4c6c56a88a88ed2f0e4932b4924d21c
Álvaro Ángel Molina <37968648+alvaretto@users.noreply.github.com>
2025-08-06 12:49:14 -0500

    Generando y optimizando gastos_carro_graficas_comparacion_interpretacion_representacion_n2_op*_v1.Rmd

--- Lab-Manjaro/01-S1-2024B/SemilleroUnico_v2.R
+++ Lab-Manjaro/01-S1-2024B/SemilleroUnico_v2.R
@@ -5,7 +5,7 @@ library(exams)
 .exams_generation_mode <- TRUE
 
 # Definición del archivo de examen y configuración inicial
-archivo_examen <- "gastos_carro_graficas_comparacion_interpretacion_representacion_n2_opA_v1.Rmd"
+archivo_examen <- "gastos_carro_graficas_comparacion_interpretacion_representacion_n2_opC_v1.Rmd"
 copias <- 1
 numpreg <- 1
 semilla <- sample(100:1e8, 1)
